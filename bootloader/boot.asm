1b3bd8181344a7f73c12307a94e552ae19418f75
llGuy <luc.rosenzweig@yahoo.com>
2021-04-13 11:36:17 +0100

    Learning about bootloader just to clear up on real-mode

---
+++ bootloader/boot.asm
@@ -0,0 +1,30 @@
+;; Tell NASM this is 16-bit mode
+bits 16
+
+;; Load at address 0x7c00
+org 0x7c00
+  
+boot:  
+  ;; Point si to hello label
+  mov si, hello
+  ;; 0x0e = Write Character in TTY mode
+  mov ah, 0x0e
+.loop:
+  lodsb
+  or al, al
+  jz halt
+  ;; BIOS interrupt 0x10 - Video services
+  int 0x10
+  jmp .loop
+halt: 
+  ;; Clear interrupts
+  cli
+  ;; Halt
+  hlt
+
+hello:  db "Hello World", 0
+
+;; Pad remaining 510 bytes with zeroes
+times 510 - ($-$$) db 0
+;; Magic bootloader - marks this 512 byte sector as bootable
+dw 0xaa55
